dc9d040c37ffaf33000acf57de5a1a23adeabb0e
Matt Miller <matt123miller@hotmail.co.uk>
2021-07-15 23:33:28 +0100

    Trying to set up testing while bringing the prettier setup from others

---
+++ jest-preprocess.js
@@ -0,0 +1,5 @@
+const babelOptions = {
+  presets: ['babel-preset-gatsby', ['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript']
+};
+
+module.exports = require('babel-jest').default.createTransformer(babelOptions);
