747e268ffbec3ee2adeecfd5c9e31055c306a884
Matt Miller <matt123miller@hotmail.co.uk>
2021-10-08 18:59:08 +0100

    Apparently this file wanted to be prettified

--- src/components/LinkCard/index.tsx
+++ src/components/LinkCard/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactChildren } from 'react';
-import {Link} from 'gatsby';
+import { Link } from 'gatsby';
 
 interface Props {
   link: string;
@@ -8,34 +8,35 @@ interface Props {
 }
 
 const LinkCard: React.FC<Props> = ({ link, children, internal, center }) => {
-
- 
   if (internal) {
-    return(
+    return (
       <Link to={link}>
-        <div className={`link-card-content card-hover ${center ? 'items-center' : ''}`} >
+        <div
+          className={`link-card-content card-hover ${
+            center ? 'items-center' : ''
+          }`}
+        >
           {children}
         </div>
       </Link>
     );
   }
-  else {
-    return (
-      <a className="mr-2" href={link} rel="noreferrer noopener" target="_blank">
-        <div className={`link-card-content card-hover ${center ? 'items-center' : ''}`} >
-          {children}
-        </div>
-      </a>
-    );
-  }
-}
 
-const InternalLink = () => {
+  return (
+    <a className="mr-2" href={link} rel="noreferrer noopener" target="_blank">
+      <div
+        className={`link-card-content card-hover ${
+          center ? 'items-center' : ''
+        }`}
+      >
+        {children}
+      </div>
+    </a>
+  );
+};
 
-}
+const InternalLink = () => {};
 
-const ExternalLink = () => {
-
-}
+const ExternalLink = () => {};
 
 export default LinkCard;
