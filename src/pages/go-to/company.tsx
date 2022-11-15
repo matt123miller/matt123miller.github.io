ca03dfe1cefb84d4d3f20e525a6ebdf9aefef823
Matt Miller <matt@m11r.dev>
2022-11-15 18:39:49 +0000

    Also maybe fixing the build error?

--- src/pages/go-to/company.tsx
+++ src/pages/go-to/company.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
 
-const RedirectToLinkedin: React.FC = () => {
-  window.location.href = 'https://www.linkedin.com/company/m11r-software/';
+const RedirectToLinkedin: React.FC = ({ url }: any) => {
+  window.location.href = url;
   return null;
 };
+
+export async function getStaticProps(context: any) {
+  return {
+    props: {
+      url: 'https://www.linkedin.com/company/m11r-software/',
+    },
+  };
+}
 
 export default RedirectToLinkedin;
