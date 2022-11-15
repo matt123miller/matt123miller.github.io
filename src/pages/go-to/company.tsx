d4bff23f0d7932fe6aa85f66b55bf027655b9583
Matt Miller <matt@m11r.dev>
2022-11-15 18:42:11 +0000

    Last change company page

--- src/pages/go-to/company.tsx
+++ src/pages/go-to/company.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
 
-const RedirectToLinkedin: React.FC = ({ url }: any) => {
-  window.location.href = url;
-  return null;
-};
+const RedirectToLinkedin: React.FC = ({ url }: any) => (
+  // window.location.href = url;
+  // return null;
+  <div>wip....</div>
+);
 
 export async function getStaticProps(context: any) {
   return {
