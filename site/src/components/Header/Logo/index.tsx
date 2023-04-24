9287bf12b171bf93786f17ce584d0fa7b461e508
Matt Miller <matt@m11r.dev>
2023-04-24 15:51:26 +0100

    Migrated the layout file and it's imports to astro

---
+++ site/src/components/Header/Logo/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+
+import { ImageSharpFluid } from 'helpers/definitions';
+
+const Logo: React.FC = () => {
+  const { site, placeholderImage } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      placeholderImage: file(
+        relativePath: { eq: "Matt_miller_portrait_mask_optimised.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 80) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  const logoTitle: string = site.siteMetadata.title;
+  const logoImage: ImageSharpFluid = placeholderImage.childImageSharp.fluid;
+
+  return (
+    <Link className="flex items-center mr-auto" to="/">
+      <figure className="logo-image">
+        <Img
+          className="border-1 rounded-full"
+          fluid={logoImage}
+          alt={logoTitle}
+        />
+      </figure>
+      <h1 className="text-lg animated-link text-xl">Matt Miller</h1>
+    </Link>
+  );
+};
+
+export default Logo;
---
+++ site/src/constants.js
@@ -0,0 +1,23 @@
+import { name, description } from "./package";
+
+const baseUrl = "https://matt123miller.dev";
+const github = "https://github.com/matt123miller";
+const siteRepo = `${github}/matt123miller.github.io`;
+const twitter = "https://twitter.com/matt123miller";
+const linkedin = "https://www.linkedin.com/in/matt123miller/";
+const email = "matt@m11r.dev";
+
+export const links = {
+  github,
+  siteRepo,
+  twitter,
+  linkedin,
+  email,
+  baseUrl,
+};
+
+export default {
+  links,
+  description,
+  name,
+};
