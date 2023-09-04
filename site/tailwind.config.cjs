2f384635ea3e8d218d747887f6d64cc337e4e692
Matt Miller <matt@m11r.dev>
2023-09-04 12:16:19 +0100

    Trying to add my silly colourful spin animation to tailwind.config.js

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 2s linear infinite",
+        "lerp-colour": "lerp-colour",
+        "spin-logo": "lerp-colour, spin 2s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
