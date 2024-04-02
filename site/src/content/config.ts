035dc7a739848a472a7bd6ca97204c9367e7a93a
Matt Miller <matt@m11r.dev>
2024-04-02 22:28:24 +0100

    refactor: Change structure of blog collection, create template page

--- site/src/content/config.ts
+++ site/src/content/config.ts
@@ -1,11 +1,22 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
   type: "content",
-  schema: {
+  schema: z.object({
     /// ???
-  },
+    coverImage: z.object({
+      src: z.string(),
+      alt: z.string(),
+      attribution: z.string(),
+    }),
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+    relatedPosts: z.array(reference("blog")).optional(),
+    published: z.boolean(),
+  }),
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
