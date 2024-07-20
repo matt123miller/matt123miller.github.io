426adb507be4a6dd8cbff5d58d564711fcd0879b
Matt Miller <matt@m11r.dev>
2024-07-20 11:04:16 +0100

    feat: edit blogpost on github works, but not dynamic enough

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ import { description, name } from "./package";
 
 const baseUrl = "https://matt123miller.dev";
 const github = "https://github.com/matt123miller";
-const siteRepo = `${github}/matt123miller.github.io`;
+const siteRepo = `${github}/matt123miller.dev`;
 const twitter = "https://twitter.com/matt123miller";
 const mastodon = "https://hachyderm.io/@m11r";
 const linkedin = "https://www.linkedin.com/in/matt123miller/";
