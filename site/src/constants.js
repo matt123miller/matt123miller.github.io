36aab9b8134f4c3f3e3f9ab2916ddbf6f52e8ef4
Matt Miller <matt@m11r.dev>
2024-07-20 09:57:13 +0100

    chore: formatting css, little tidying, adding mastodon to link constants

--- site/src/constants.js
+++ site/src/constants.js
@@ -1,9 +1,10 @@
-import { name, description } from "./package";
+import { description, name } from "./package";
 
 const baseUrl = "https://matt123miller.dev";
 const github = "https://github.com/matt123miller";
 const siteRepo = `${github}/matt123miller.github.io`;
 const twitter = "https://twitter.com/matt123miller";
+const mastodon = "https://hachyderm.io/@m11r";
 const linkedin = "https://www.linkedin.com/in/matt123miller/";
 const email = "matt@m11r.dev";
 
@@ -11,6 +12,7 @@ export const links = {
   github,
   siteRepo,
   twitter,
+  mastodon,
   linkedin,
   email,
   baseUrl,
